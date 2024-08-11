76ca5f706cbd130c49f9ef4194202f674228f898
yukinko05 <grey.sora.35@gmail.com>
2024-08-11 17:26:27 +0900

    Station13を完了

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  let checkbox = document.getElementById("check");
+  let p = document.getElementById("text");
+  console.log(checkbox.checked);
+  if (checkbox.checked) {
+    p.style.backgroundColor = "red";
+  } else {
+    p.style.backgroundColor = "";
+  }
 }
